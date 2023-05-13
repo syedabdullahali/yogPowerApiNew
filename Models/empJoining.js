418f1bc8bdcc7aa716dca7b4a01d916171a792b2
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-13 14:21:05 +0530

    Update empJoining.js

--- Models/empJoining.js
+++ Models/empJoining.js
@@ -2,11 +2,8 @@ let mongoose = require('mongoose')
 
 let empJoining = new mongoose.Schema({
 username: String, 
-empName:String,
-offerLatter: String,
-ApponmentLater: String,
-Indexion:String,  
-employeeId:String,
+DocumentName:String,
+documentDetails:String,    
 }, { timestamps: true })
 
 module.exports =   mongoose.model('empJoining', empJoining);
