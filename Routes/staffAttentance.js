6964ae4b13883e1b35c7de260d131cf71209f293
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-16 01:50:11 +0530

    Update staffAttentance.js

--- Routes/staffAttentance.js
+++ Routes/staffAttentance.js
@@ -16,27 +16,25 @@ router.get('/report', async function (req, res) {
     try {
         const response = await staffAttendance.find({status:'Done'})
         
-         const data = response.data.filter((el)=>el.classesId)
+         const data = response.filter((el)=>el.classesId)
 
-//                 const uniqObj = []
+                const uniqObj = []
 
-//                data.forEach((el,i) => {
-//                  console.log(i)
-//                  if(!uniqObj.some((el2)=>el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId)){
-//                     console.log(el)
-//                       uniqObj.push({classesId:el.classesId,staffId:el.staffId,
-//                         time:{hours:+el.totalWorkinghour.split(":")[0],mins:+el.totalWorkinghour.split(":")[1]},details:el
-//                     })
-//                  }else{
-//                    uniqObj.forEach((el2,i)=>{
-//                     if(el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId){
-//                         el2.time.hours+=+el.totalWorkinghour.split(":")[0]
-//                         el2.time.mins+=+el.totalWorkinghour.split(":")[1]
+               response.forEach((el,i) => {
+                 if(!uniqObj.some((el2)=>el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId)){
+                      uniqObj.push({classesId:el.classesId,staffId:el.staffId,
+                        time:{hours:+el.totalWorkinghour.split(":")[0],mins:+el.totalWorkinghour.split(":")[1]},details:el
+                    })
+                 }else{
+                   uniqObj.forEach((el2,i)=>{
+                    if(el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId){
+                        el2.time.hours+=+el.totalWorkinghour.split(":")[0]
+                        el2.time.mins+=+el.totalWorkinghour.split(":")[1]
 
-//                     }
-//                    })
-//                  }
-//                });  
+                    }
+                   })
+                 }
+               });  
 
         
         return res.status(200).json(data);
