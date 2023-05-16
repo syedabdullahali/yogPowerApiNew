89c46fecb65540e83738b74c568d202b2fd17436
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-16 15:58:58 +0530

    Update trainerSalarySlip.js

--- Models/trainerSalarySlip.js
+++ Models/trainerSalarySlip.js
@@ -3,25 +3,12 @@ let mongoose = require('mongoose')
 
 let trainerSalarySlip = new mongoose.Schema({
     username: String,
-    staffId:String,
-    attentanceId: String,
-    StaffName: String,
-    staffContact:String,
-    centerId: String,
-    checkDate: Date,
-    checkIn: String,
-    checkOut: String,
-    ContactNo: String,
-    Department: String,
-    Designation: String,
-    EmployeeCategory: String,
-    joiningDate: String,
-    empCheckInId:String,
-    shiftTimeing:  String,
-    shiftStartTime:  String,
-    shiftEndTime: String,
-    totalWorkinghour: String,
-    status: String,
+    serviceName:String,
+    trainerName:String,
+    categoryName:String,
+    batchDuration:String,
+    batchTiming:String,
+    typeOfTrainer:String
 }, { timestamps: true })
 
 module.exports = mongoose.model('trainerSalarySlip', trainerSalarySlip);
