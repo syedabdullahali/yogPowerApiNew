faecdb64ebb0cec166a53581d00ebfd6fe4906fb
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-16 13:00:41 +0530

    Update staffAttentance.js

--- Routes/staffAttentance.js
+++ Routes/staffAttentance.js
@@ -15,28 +15,28 @@ router.get('/all', async function (req, res) {
 router.get('/report', async function (req, res) {
     try {
         const response = await staffAttendance.find({status:'Done'})
-        
-         const data = response.filter((el)=>Boolean(el.classesId))
+        const data = [...res.data.filter((el)=>Boolean(el.classesId&&el.status ==='Done'))]
 
-                const uniqObj = []
+            const uniqObj = []
 
-               data.forEach((el,i) => {
-                 if(!uniqObj.some((el2)=>el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId)){
-                      uniqObj.push({classesId:el.classesId,staffId:el.staffId,
-                        time:{hours:+el.totalWorkinghour.split(":")[0],mins:+el.totalWorkinghour.split(":")[1]},details:el,noOfClasses:1
-                    })
-                 }else{
-                   uniqObj.forEach((el2,i)=>{
-                    if(el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId){
-                        el2.time.hours+=+el.totalWorkinghour.split(":")[0]
-                        el2.time.mins+=+el.totalWorkinghour.split(":")[1]
-                        el2.noOfClasses+=1
-                    }
-                   })
-                 }
-               });  
+           data.forEach((el,i) => {
+             if(!uniqObj.some((el2)=>el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId&&
+                              el2?.month===new Date(el.checkDate).getMonth()&&el2?.year===new Date(el.checkDate).getFullYear())){
+                  uniqObj.push({classesId:el.classesId,staffId:el.staffId,month:new Date(el.checkDate)
+                    .getMonth(),year:new Date(el.checkDate).getFullYear(),                                  
+                    time:{hours:+el.totalWorkinghour.split(":")[0],mins:+el.totalWorkinghour.split(":")[1]},details:el,noOfClasses:1
+                })
+             }else{
+               uniqObj.forEach((el2,i)=>{
+                if(el?.classesId===el2?.classesId&&el?.staffId===el2?.staffId &&el2.month===new Date(el.checkDate).getMonth()&&el2.year===new Date(el.checkDate).getFullYear()){
+                    el2.time.hours+=+el.totalWorkinghour.split(":")[0]
+                    el2.time.mins+=+el.totalWorkinghour.split(":")[1]
+                    el2.noOfClasses+=1
+                }
+               })
+             }
+           });  
 
-        
         return res.status(200).json(uniqObj);
     } catch (err) {
         return res.status(500).json({ error: err })
